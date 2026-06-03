fe0e8af3be3d252e043d50a3e37808265e92f660
Venky Matena <syamvenkatesh910@gmail.com>
2026-06-03 12:04:50 +0530

    small fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # Allow requests from all Render.com domains
-    origins 'twitter24-htj4.onrender.com', 'twitter24-be.onrender.com'
+    origins '*'
 
     # In development, allow localhost
-    origins 'localhost:3000', 'localhost:3001' if Rails.env.development?
+    origins '*' if Rails.env.development?
 
     resource '*',
       headers: :any,
